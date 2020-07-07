5578a56245cbf8ec964faea007eaa493a53c9471
Priyanka <sweeti270789@gmail.com>
2020-07-07 12:09:51 +0530

    move to public

---
+++ public/src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="hw-header site-header--wrapper">
+<nav class="navbar navbar-default" role="navigation" data-module="nav-tabs">
+<div class="container-fluid">
+ <img class="hw-logo" src="https://ak-secure.hotwirestatic.com/current/static/images/branding/hw-logos/hotwire-logo.svg"/>
+</div>
+</nav>
+</div>
+`;
